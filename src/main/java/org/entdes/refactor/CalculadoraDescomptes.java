6a16f28a9ac9945d5743f743bc3a8aed13740133
Raúl Morejón <raulmorejon@iesebre.com>
2025-02-27 10:13:45 +0100

    agefit

--- src/main/java/org/entdes/refactor/CalculadoraDescomptes.java
+++ src/main/java/org/entdes/refactor/CalculadoraDescomptes.java
@@ -4,26 +4,31 @@ public class CalculadoraDescomptes {
 
     public int calcularDescompteComanda(int preu, int quantitat) {
         // Validar entrada
-        if (preu <= 0 || quantitat <= 0) {
-            return 0;
-        }
+        if(!validarEntrada(preu, quantitat))return 0; 
         // Calcular descompte
-        int descompte = (preu * quantitat) * 10 / 100;
-        // Retornar descompte
-        return descompte;
+        return calcularDescompte(preu, quantitat, 10);
     }
 
     public int calcularDescompteDevolucio(int preu, int quantitat) {
         // Validar entrada
-        if (preu <= 0 || quantitat <= 0) {
-            return 0;
-        }
+        if(!validarEntrada(preu, quantitat))return 0; 
         // Calcular descompte
-        int descompte = (preu * quantitat) * 5 / 100;
-        // Retornar descompte
+        return calcularDescompte(preu, quantitat, 5);
+    }
+
+    
+    public boolean validarEntrada(int preu, int quantitat){
+        if (preu <= 0 || quantitat <= 0) return false;
+        return true;
+    }
+
+    public int calcularDescompte (int preu, int quantitat, int num){
+        int descompte = (preu * quantitat) * num / 100;
         return descompte;
     }
 
+    
+    
     public static void main(String[] args) {
         CalculadoraDescomptes calc = new CalculadoraDescomptes();
 

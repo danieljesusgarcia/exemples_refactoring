fd389139763cef056eb5abea8238e53d2978b1d6
ivelyckho-hub <illyavelychko@iesebre.com>
2025-02-27 10:50:22 +0100

    Refactorització del codi

--- src/main/java/org/entdes/refactor/CalculadoraDescomptes.java
+++ src/main/java/org/entdes/refactor/CalculadoraDescomptes.java
@@ -2,27 +2,32 @@ package org.entdes.refactor;
 
 public class CalculadoraDescomptes {
 
+
     public int calcularDescompteComanda(int preu, int quantitat) {
         // Validar entrada
-        if (preu <= 0 || quantitat <= 0) {
+        if (!entradavalida(preu, quantitat)) {
             return 0;
         }
-        // Calcular descompte
-        int descompte = (preu * quantitat) * 10 / 100;
-        // Retornar descompte
-        return descompte;
+        return calculardescompte(preu, quantitat, 10);
     }
 
     public int calcularDescompteDevolucio(int preu, int quantitat) {
         // Validar entrada
-        if (preu <= 0 || quantitat <= 0) {
+        if (!entradavalida(preu, quantitat)) {
             return 0;
         }
         // Calcular descompte
-        int descompte = (preu * quantitat) * 5 / 100;
-        // Retornar descompte
-        return descompte;
+        return calculardescompte(preu, quantitat, 5);
+       
     }
+    // He afegir els metodes privats entradavalida (per no repetir el codi de validar la entrada) i calculardescompte (per no repetir el codi de calcular el descompte)
+    private boolean entradavalida(int preu, int quantitat) {
+        return preu > 0 && quantitat > 0;
+    }
+
+    private int calculardescompte(int preu, int quantitat, int percentatge) {
+        return (preu * quantitat) * percentatge / 100;
+     }
 
     public static void main(String[] args) {
         CalculadoraDescomptes calc = new CalculadoraDescomptes();
